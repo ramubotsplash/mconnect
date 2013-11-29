33d2cfb4d4c605311847f299f1219960ae50270d
Aarpy <ramu@rohilabs.com>
2013-11-29 07:44:53 -0500

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mconnect::Application.config.secret_key_base = '45d556996b429ade6effa695e786dc1aa23e2f41382d98ed339d7a731f15f93abeed66bd7a0d9b87f4ab79f5a8254a831b031bc9b489313f495b0bc4c6c8577b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mconnect::Application.config.session_store :cookie_store, key: '_mconnect_session'
